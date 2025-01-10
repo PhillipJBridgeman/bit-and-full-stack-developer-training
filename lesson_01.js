819d85e346127cbc728e671dfc370599df5e174b
PhillipJBridgeman <106999026+PhillipJBridgeman@users.noreply.github.com>
2025-01-10 07:34:41 -0600

    Added Lesson 01 of the udemy course.

---
+++ lesson_01.js
@@ -0,0 +1,17 @@
+/* Modern JavaScript (Complete guide, from novice to ninja)
+    Author: Shaun Pelling
+    Student: Phillip Bridgeman
+    Date: January 10, 2025
+    Description: This is the first lesson in the Udemy course "Modern JavaScript" by Shaun Pelling.
+*/
+
+/* Lesson 1: Introduction to Modern JavaScript */
+
+/* JavaScript is a general purpose programming language that is used to create interactive websites. It is a high-level, interpreted language that is easy to learn and use. JavaScript is a client-side language, meaning that it runs on the user's computer, rather than on the server. This allows JavaScript to interact with the user's browser and create dynamic, interactive web pages. */
+
+/* Focus of the course:
+- Front End Web Development
+- Using JavaScript to create interactive websites
+- Modern JavaScript features and best practices
+- ES6 and beyond
+*/
